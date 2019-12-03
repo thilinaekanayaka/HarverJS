8b43a820c3d41a4c05b0e3e6edcdcb01862fc691
Thilina Ekanayaka <thilinas@eight25media.com>
2019-12-03 13:27:21 +0530

    Converted to async functions

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ contact : +94774657948
 
 const { getRandomWordSync, getRandomWord } = require("word-maker");
 
-//Q1
-for (let i = 1; i < 101; i++) {
+// Q1;
+for (let i = 1; i < 101; i++)
   console.log(i + ": " + getRandomWordSync({ withErrors: false }));
-}
 
-//Q2
+// Q2
 for (let i = 1; i < 101; i++) {
   if (i % 3 == 0 && i % 5 != 0) console.log(i + ": Fizz");
   else if (i % 5 == 0 && i % 3 != 0) console.log(i + ": Buzz");
@@ -19,34 +18,25 @@ for (let i = 1; i < 101; i++) {
   else console.log(i + ": " + getRandomWordSync({ withErrors: false }));
 }
 
-//Q3 - Part A
-for (let i = 1; i < 101; i++) {
-  let promise = new Promise(function(resolve, reject) {
-    resolve(getRandomWord({ withErrors: false }));
-  });
-
-  promise.then(function(value) {
-    console.log(i + ": " + value);
-  });
+// Q3 - Part A
+async function printRandomWord() {
+  for (let i = 1; i < 101; i++)
+    console.log(i + ": " + (await getRandomWord({ withErrors: false })));
 }
+printRandomWord();
 
-//Q3 - Part B
-for (let i = 1; i < 101; i++) {
-  if (i % 3 == 0 && i % 5 != 0) console.log(i + ": Fizz");
-  else if (i % 5 == 0 && i % 3 != 0) console.log(i + ": Buzz");
-  else if (i % 3 == 0 && i % 5 == 0) console.log(i + ": FizzBuzz");
-  else {
-    let promise = new Promise(function(resolve, reject) {
-      resolve(getRandomWord({ withErrors: false }));
-    });
-
-    promise.then(function(value) {
-      console.log(i + ": " + value);
-    });
+// Q3 - Part B
+async function printFizzBuzz() {
+  for (let i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 != 0) console.log(i + ": Fizz");
+    else if (i % 5 == 0 && i % 3 != 0) console.log(i + ": Buzz");
+    else if (i % 3 == 0 && i % 5 == 0) console.log(i + ": FizzBuzz");
+    else console.log(i + ": " + (await getRandomWord({ withErrors: false })));
   }
 }
+printFizzBuzz();
 
-//Q4 - Part A
+// Q4 - Part A
 for (let i = 1; i < 101; i++) {
   try {
     if (i % 3 == 0 && i % 5 != 0) console.log(i + ": Fizz");
@@ -59,21 +49,16 @@ for (let i = 1; i < 101; i++) {
 }
 
 //Q4 - Part B
-for (let i = 1; i < 101; i++) {
-  if (i % 3 == 0 && i % 5 != 0) console.log(i + ": Fizz");
-  else if (i % 5 == 0 && i % 3 != 0) console.log(i + ": Buzz");
-  else if (i % 3 == 0 && i % 5 == 0) console.log(i + ": FizzBuzz");
-  else {
-    let promise = new Promise(function(resolve, reject) {
-      resolve(getRandomWord({ withErrors: true }));
-    });
-
-    promise
-      .then(function(value) {
-        console.log(i + ": " + value);
-      })
-      .catch(function() {
-        console.log(i + ": It shouldn't break anything!");
-      });
+async function printFizzBuzzWithError() {
+  for (let i = 1; i < 101; i++) {
+    try {
+      if (i % 3 == 0 && i % 5 != 0) console.log(i + ": Fizz");
+      else if (i % 5 == 0 && i % 3 != 0) console.log(i + ": Buzz");
+      else if (i % 3 == 0 && i % 5 == 0) console.log(i + ": FizzBuzz");
+      else console.log(i + ": " + (await getRandomWord({ withErrors: true })));
+    } catch (e) {
+      console.log(i + ": It shouldn't break anything!");
+    }
   }
 }
+printFizzBuzzWithError();
